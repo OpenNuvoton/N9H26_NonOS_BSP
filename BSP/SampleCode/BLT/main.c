5075fc1244357af91dd412ea40ea9e06cacc6ef2
YCYen0 <YCYen0@nuvoton.com>
2019-08-13 16:33:51 +0800

    2019/08/13 Routine business for weekly rebuild and check.

--- BSP/SampleCode/BLT/main.c
+++ BSP/SampleCode/BLT/main.c
@@ -23,7 +23,11 @@ static uint8_t src_pat[] = {
 
 #define SIZE_TXMEM          (SIZE_SRCIMG_BUF + SIZE_DISP_BUF)
 
+#if defined (__GNUC__)
+uint8_t txmem[SIZE_TXMEM] __attribute__((aligned (32)));
+#else
 __align (32) uint8_t txmem[SIZE_TXMEM];
+#endif
 
 /* To avoid error-prone cache synchronization, the txmem for CPU and BLT operation is always
  * non-cacheable. */
@@ -84,13 +88,13 @@ void clr_disp_buf(uint32_t fill_color)
   * Inv(Ms) = | 0    1/sy 0 |  Inv(Mt) = | 0 1 -ty |  Inv(Ma) = | 0 1 0.5 |
   *           \ 0    0    1 /            \ 0 0 1   /            \ 0 0 1   /
   *
-  *                               / 1/sx 0    -(1/sx)*tx+0.5*(a+b) \   / a b src.xoffset \
-  * Inv(Ms) * Inv(Mt) * Inv(Ma) = | 0    1/sy -(1/sy)*ty+0.5*(c+d) | = | c d src.yoffset |
+  *                               / 1/sx 0    -(1/sx)*tx+0.5*(a+c) \   / a c src.xoffset \
+  * Inv(Ms) * Inv(Mt) * Inv(Ma) = | 0    1/sy -(1/sy)*ty+0.5*(b+d) | = | b d src.yoffset |
   *                               \ 0    0    1                    /   \ 0 0 0           /
   */
 void demo_scale(float ox, float oy, float sx, float sy, int is_tiling)
 {
-    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operaiton.
+    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operation.
     bltSetDisplayFormat(FMT_DST);           // Set destination format.
     bltSetSrcFormat(eDRVBLT_SRC_ARGB8888);  // Set source image format to RGB888/ARGB8888.
     bltSetRevealAlpha(eDRVBLT_EFFECTIVE);   // Set source image format to premultiplied alpha.
@@ -143,13 +147,13 @@ void demo_scale(float ox, float oy, float sx, float sy, int is_tiling)
             
             // Apply amendment to mapping point error.
             bltGetTransformMatrix(&xform_mx);
-            src_img.i32XOffset += (xform_mx.a + xform_mx.b) / 2;
-            src_img.i32YOffset += (xform_mx.c + xform_mx.d) / 2;
+            src_img.i32XOffset += (xform_mx.a + xform_mx.c) / 2;
+            src_img.i32YOffset += (xform_mx.b + xform_mx.d) / 2;
         }
         src_img.i16Width = SRCIMG_WIDTH;
         src_img.i32Stride = SRCIMG_STRIDE;
         src_img.i16Height = SRCIMG_HEIGHT;
-        
+    
         bltSetSrcImage(src_img);
     }
     
@@ -191,13 +195,13 @@ void demo_scale(float ox, float oy, float sx, float sy, int is_tiling)
   * Inv(Mr) = | -sin£c con£c 0 |  Inv(Mt) = | 0 1 -ty |  Inv(Ma) = | 0 1 0.5 |
   *           \ 0     0    1 /            \ 0 0 1   /            \ 0 0 1   /
   *
-  *                               / con£c  sin£c -con£c*tx-sin£c*ty+0.5*(a+b) \   / a b src.xoffset \
-  * Inv(Mr) * Inv(Mt) * Inv(Ma) = | -sin£c con£c sin£c*tx-con£c*ty+0.5*(c+d)  | = | c d src.yoffset |
+  *                               / con£c  sin£c -con£c*tx-sin£c*ty+0.5*(a+c) \   / a c src.xoffset \
+  * Inv(Mr) * Inv(Mt) * Inv(Ma) = | -sin£c con£c sin£c*tx-con£c*ty+0.5*(b+d)  | = | b d src.yoffset |
   *                               \ 0     0    1                          /   \ 0 0 0           /
   */
 void demo_rotate(float ox, float oy, float deg)
 {
-    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operaiton.
+    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operation.
     bltSetDisplayFormat(FMT_DST);           // Set destination format.
     bltSetSrcFormat(eDRVBLT_SRC_ARGB8888);  // Set source image format to RGB888/ARGB8888.
     bltSetRevealAlpha(eDRVBLT_EFFECTIVE);   // Set source image format to premultiplied alpha.
@@ -251,8 +255,8 @@ void demo_rotate(float ox, float oy, float deg)
             
             // Apply amendment to mapping point error.
             bltGetTransformMatrix(&xform_mx);
-            src_img.i32XOffset += (xform_mx.a + xform_mx.b) / 2;
-            src_img.i32YOffset += (xform_mx.c + xform_mx.d) / 2;
+            src_img.i32XOffset += (xform_mx.a + xform_mx.c) / 2;
+            src_img.i32YOffset += (xform_mx.b + xform_mx.d) / 2;
         }
         src_img.i16Width = SRCIMG_WIDTH;
         src_img.i32Stride = SRCIMG_STRIDE;
@@ -299,13 +303,13 @@ void demo_rotate(float ox, float oy, float deg)
   * Inv(Mm) = | 0       mx?-1:1 0 |  Inv(Mt) = | 0 1 -ty |  Inv(Ma) = | 0 1 0.5 |
   *           \ 0       0       1 /            \ 0 0 1   /            \ 0 0 1   /
   *
-  *                               / my?-1:1 0       -(my?-1:1)*tx+0.5*(a+b) \   / a b src.xoffset \
-  * Inv(Mr) * Inv(Mt) * Inv(Ma) = | 0       mx?-1:1 -(mx?-1:1)*ty+0.5*(c+d) | = | c d src.yoffset |
+  *                               / my?-1:1 0       -(my?-1:1)*tx+0.5*(a+c) \   / a c src.xoffset \
+  * Inv(Mm) * Inv(Mt) * Inv(Ma) = | 0       mx?-1:1 -(mx?-1:1)*ty+0.5*(b+d) | = | b d src.yoffset |
   *                               \ 0       0       1                       /   \ 0 0 0           /
   */
 void demo_reflect(float ox, float oy, int mx, int my)
 {
-    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operaiton.
+    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operation.
     bltSetDisplayFormat(FMT_DST);           // Set destination format.
     bltSetSrcFormat(eDRVBLT_SRC_ARGB8888);  // Set source image format to RGB888/ARGB8888.
     bltSetRevealAlpha(eDRVBLT_EFFECTIVE);   // Set source image format to premultiplied alpha.
@@ -359,8 +363,8 @@ void demo_reflect(float ox, float oy, int mx, int my)
             
             // Apply amendment to mapping point error.
             bltGetTransformMatrix(&xform_mx);
-            src_img.i32XOffset += (xform_mx.a + xform_mx.b) / 2;
-            src_img.i32YOffset += (xform_mx.c + xform_mx.d) / 2;
+            src_img.i32XOffset += (xform_mx.a + xform_mx.c) / 2;
+            src_img.i32YOffset += (xform_mx.b + xform_mx.d) / 2;
         }
         src_img.i16Width = SRCIMG_WIDTH;
         src_img.i32Stride = SRCIMG_STRIDE;
@@ -388,7 +392,7 @@ void demo_reflect(float ox, float oy, int mx, int my)
 
 void demo_alpha(float ox, float oy, float alpha)
 {
-    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operaiton.
+    bltSetFillOP((E_DRVBLT_FILLOP) FALSE);  // Blit operation.
     bltSetDisplayFormat(FMT_DST);           // Set destination format.
     bltSetSrcFormat(eDRVBLT_SRC_ARGB8888);  // Set source image format to RGB888/ARGB8888.
     bltSetRevealAlpha(eDRVBLT_EFFECTIVE);   // Set source image format to premultiplied alpha.
@@ -498,7 +502,7 @@ int main()
     }
 
     sysSetLocalInterrupt (ENABLE_IRQ);  // Enable CPSR I bit
-    
+
     do {
         
         memcpy((void *) ADDR_SRCIMG, src_pat, SIZE_SRCIMG);
